a0bcb7a59dad8f9508f5ae16845f802e46977984
saams4u <mahjouri.saamahn@gmail.com>
2020-07-08 22:01:29 -0500

    added more sources

---
+++ packages/api/run.py
@@ -0,0 +1,7 @@
+from source.app import create_app
+from source.config import DevelopmentConfig, ProductionConfig
+
+application = create_app(config_object=ProductionConfig)
+
+if __name__ == '__main__':
+    application.run()
